79c2c8cbd351635180f562565a35a7b25b61c796
Ade Attwood <ade@practically.io>
2021-08-19 19:53:37 +0100

    refactor: add ^ to the end of the cherry picked commit
    Using the ^ at the end git logs lower bound is not inclusive.

--- src/scripts/lint.sh
+++ src/scripts/lint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-#!/bin/bash
 if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 $SUDO npm install -g @commitlint/cli "$CL_PARAM_CONFIGS"
 
@@ -16,18 +14,12 @@ fi
 # If there is only one commit, set target_head to that commit
 if [ "$(echo "$git_log" | wc -l | xargs)" == "1" ]; then
   target_head=""
-  echo "Target Head = $target_head"
 elif [ "$current_branch" != "$target_branch" ]; then
-  echo "Setting target to $target_branch"
-  target_head="$(git cherry "$target_branch" | head -1 | cut -d " " -f2-)"
-  echo "Target Head = $target_head"
+  # Using the ^ at the end git logs lower bound is not inclusive
+  target_head="$(git cherry "$target_branch" | head -1 | cut -d " " -f2-)^"
 else
-  echo "Setting target to HEAD -1"
   commit="$(echo "$git_log" | head -1)"
   target_head="$(git log "$commit^" -1 pretty=%H)"
 fi
-set +x
-commitlint -v
+
 commitlint --verbose --config "$CL_PARAM_CONFIG_PATH" --from="$target_head"
-echo $?
-echo "Ended"
